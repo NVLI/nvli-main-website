2e33515f1956effd9d13a3a3dbd63af40ed144b0
Safallia Joseph <safallia.joseph@zyxware.com>
2016-07-07 13:43:32 +0530

    NVLI-40 Add the mirador module

---
+++ docroot/modules/contrib/mirador/js/mirador-load.js
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * Helper js to load the mirador viewer.
+ */
+
+(function ($, Drupal) {
+
+  Drupal.behaviors.Mirador = {
+    attach: function (context, settings) {
+      var manifestUri = settings.init.entity.manifest_uri;
+      var viewerID = settings.init.entity.viewer_id;
+      if ($('#' + viewerID + ' .mirador-viewer').length == 0) {
+        Mirador({
+          "id": viewerID,
+          "layout": "1x1",
+          'openManifestsPage' : false,
+          'showAddFromURLBox' : false,
+          "saveSession": false,
+          "data": [
+            { "manifestUri": manifestUri, "location": "National Virtual Library Of India"},
+          ],
+          "windowObjects":[{
+            "loadedManifest" : manifestUri,
+            "viewType" : "ImageView"} ],
+        });
+      }
+    }
+  };
+})(jQuery, Drupal);
+
