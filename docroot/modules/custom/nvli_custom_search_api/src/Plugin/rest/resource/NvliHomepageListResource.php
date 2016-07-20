648d372d7907558ce2aa48bad5b078b612b0bd68
Disha Bhadra <dishasbhadra2013@gmail.com>
2016-07-20 11:42:25 +0530

    NVLI-179 added the helper function and comments

--- docroot/modules/custom/nvli_custom_search_api/src/Plugin/rest/resource/NvliHomepageListResource.php
+++ docroot/modules/custom/nvli_custom_search_api/src/Plugin/rest/resource/NvliHomepageListResource.php
@@ -18,15 +18,16 @@ use Drupal\Core\Entity;
 use Drupal\custom_solr_search\SearchSolrAll;
 use Drupal\custom_solr_search\Search;
 use Symfony\Component\HttpFoundation;
+use Drupal\nvli_custom_search_api\EntityDetail;
 
 /**
  * Provides a resource to get view modes by entity and bundle.
  *
  * @RestResource(
- *   id = "custom_rest_homepage_listing_resource",
- *   label = @Translation("Custom rest homepage listing resource"),
+ *   id = "custom_rest_resource_type_listing_resource",
+ *   label = @Translation("Custom rest resource type listing resource"),
  *   uri_paths = {
- *     "canonical" = "/rest/v1/homepage/listing"
+ *     "canonical" = "/rest/v1/resource_type"
 
  *   }
  * )
@@ -46,6 +47,13 @@ class NvliHomepageListResource extends ResourceBase {
    * @var \Drupal\custom_solr_search\SearchSolrAll
    */
   protected $searchall;
+
+  /**
+   * \Drupal\nvli_custom_search_api\Entity ID.
+   *
+   * @var \Drupal\nvli_custom_search_api\EntityDetail
+   */
+  protected $entitydetail;
 
   /**
    * A current user instance.
@@ -71,15 +79,18 @@ class NvliHomepageListResource extends ResourceBase {
    *   A current user instance.
    * @param \Drupal\custom_solr_search\Search $search
    *   Custom Solr search service.
+   * @param \Drupal\nvli_custom_search_api\EntityDetail $entitydetail
+   *   Custom Entity.
    * @param \Drupal\custom_solr_search\SearchSolrAll $searchall
    *   Custom Solr search service for all core.
    */
   public function __construct(
-  array $configuration, $plugin_id, $plugin_definition, array $serializer_formats, LoggerInterface $logger, AccountProxyInterface $current_user, Search $search, SearchSolrAll $searchall) {
+  array $configuration, $plugin_id, $plugin_definition, array $serializer_formats, LoggerInterface $logger, AccountProxyInterface $current_user, Search $search, SearchSolrAll $searchall, EntityDetail $entitydetail) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
     $this->currentUser = $current_user;
     $this->search = $search;
     $this->searchall = $searchall;
+    $this->entitydetail = $entitydetail;
   }
 
   /**
@@ -87,7 +98,15 @@ class NvliHomepageListResource extends ResourceBase {
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
-        $configuration, $plugin_id, $plugin_definition, $container->getParameter('serializer.formats'), $container->get('logger.factory')->get('rest'), $container->get('current_user'), $container->get('custom_solr_search.search'), $container->get('custom_solr_search.search_all')
+        $configuration,
+        $plugin_id,
+        $plugin_definition,
+        $container->getParameter('serializer.formats'),
+        $container->get('logger.factory')->get('rest'),
+        $container->get('current_user'),
+        $container->get('custom_solr_search.search'),
+        $container->get('custom_solr_search.search_all'),
+        $container->get('nvli_custom_search_api.entity_detail')
     );
   }
 
@@ -112,6 +131,7 @@ class NvliHomepageListResource extends ResourceBase {
       // For each type fetch the results from solr.
       foreach ($type as $key => $value) {
         $data = array();
+        // @ TODO Once config entity available fetch the filter query.
         // Hardcoding the format for type.
         $options = '(format:"' . $value . '")';
         // Use Solr search service to fetch the results.
@@ -121,7 +141,7 @@ class NvliHomepageListResource extends ResourceBase {
           // Fetch the entity_id for each doc.
           foreach ($solr_result as $row) {
             $doc_id = $row->id;
-            $results = nvli_custom_search_get_nid($doc_id);
+            $results = $this->entitydetail->get_nid($doc_id);
             $data[] = array_merge((array) $row, $results);
             $result[$value] = $data;
           }
