34c2d9d72a169bda477aa4dc113eab0a4000749a
Ashish Dalvi <ashish.vitthal.dalvi@gmail.com>
2016-07-27 14:53:22 +0530

    NVLI-281 : Updated resource manager service to add filter by resource type condition

--- docroot/modules/custom/nvli_custom/src/Plugin/Block/ResourceListingBlock.php
+++ docroot/modules/custom/nvli_custom/src/Plugin/Block/ResourceListingBlock.php
@@ -34,17 +34,20 @@ class ResourceListingBlock extends BlockBase {
     // Fetch the query filter.
     $filterQuerySettings = \Drupal::service('custom_solr_search.filter_query_settings')->getFilterQuerySetings();
    
-    // If resource type are present.
+    // Validation to check resurce entity exist.
     if (!empty($filterQuerySettings)) {
+      
+      // Iterate resource entity array and create renderable array of resource title 
+      // and resource link.
       foreach ($filterQuerySettings as $resource_type => $config_entity) {
 
         // Generate resource type search page url based on the 
-        $url = Url::fromRoute('nvli_custom_search.resource_entity_count', array('resource_type' => $config_entity['id']));
+        $url = Url::fromRoute('nvli_custom_search.nvli_search_resource_page', array('resource_type' => $config_entity['id']));
 
         $render[] = array(
           '#theme' => 'resource_listing_block',
           '#title' => \Drupal::l(t($config_entity['label']), $url),
-          '#count' => \Drupal::service('nvli_custom.resource_count')->resourceEntityCount($resource_type),
+          '#count' => \Drupal::service('nvli_custom.resource_manager')->resourceEntityCount($resource_type),
         );
       }
     }
