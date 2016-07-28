ed68225bda894c376936648f076b3f94f543ddb6
piyuesh23 <piyuesh23@qed42.com>
2016-07-28 17:58:05 +0530

    Updating url shortener logic

--- docroot/modules/custom/nvli_shorten/src/NvliShortenManager.php
+++ docroot/modules/custom/nvli_shorten/src/NvliShortenManager.php
@@ -28,4 +28,27 @@ class NvliShortenManager {
       watchdog_exception('nvli_shorten', $e);
     }
   }
+
+  public function setShortUrls() {
+    $query = \Drupal::entityQuery('node')
+      ->condition('type', 'resource')
+      ->condition('field_text_plain_single_1.value', '', '=');
+    $result = $query->execute();
+
+    if (count($result)) {
+      $resources = \Drupal::entityManager()
+        ->getStorage('node')
+        ->loadMultiple($result);
+
+      foreach ($resources as $resource) {
+        if (!empty($resource->get('field_text_plain_single_1')->value)) {
+          $resource_id = $resource->id();
+          $source_url = 'http://dev-nvli.iitb.ac.in//node/' . $resource_id;
+          $short_url = $this->generateShortUrl($source_url);
+          $resource->set('field_text_plain_single_1', $short_url->shortUrl);
+          $resource->save();
+        }
+      }
+    }
+  }
 }
