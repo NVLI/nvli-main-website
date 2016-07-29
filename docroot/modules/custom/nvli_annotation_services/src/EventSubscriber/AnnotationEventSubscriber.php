1669e28625c1aebc2e3ac8e93060943f7c990912
aditya_anurag <adityaanurag21@gmail.com>
2016-07-29 11:52:20 +0530

    Adding annotation server module

---
+++ docroot/modules/custom/nvli_annotation_services/src/EventSubscriber/AnnotationEventSubscriber.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\nvli_annotation_services\EventSubscriber;
+
+use Drupal\nvli_annotation_services\AnnotationStoreEvent;
+use Drupal\Core\Database\Database;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+
+/**
+ * Class AnnotationEventSubscriber.
+ *
+ * @package Drupal\nvli_annotation_services
+ */
+class AnnotationEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[AnnotationStoreEvent::SAVE][] = array('reactOnAnnotationSave', 100);
+    return $events;
+
+  }
+
+  /**
+   * Subscriber Callback for the event.
+   * @param AnnotationStoreEvent $event
+   */
+  public function reactOnAnnotationSave(AnnotationStoreEvent $event) {
+
+    $id = $event->getReferenceSolrDocId();
+    $fields = $this->getAnnotationFields($id);
+    $server = 'nvli';
+    $results = \Drupal::service('nvli_annotation_services.add_annotation')
+      ->addAnnotation($server, $id, $fields);
+
+    if($results->getResponse()->getStatusMessage() == 'OK'){
+      drupal_set_message("Saved annotation for solr doc:" . $event->getReferenceSolrDocId());
+    }else{
+      drupal_set_message($results->getResponse()->getStatusMessage());
+    }
+
+  }
+  protected function getAnnotationFields($id){
+    $connection = Database::getConnection();
+    $query = $connection->select('annotation_store_entity', 'ae')
+      ->fields('ae', array('id'));
+    $query->condition('resource_ref', $id);
+    $data = $query->execute()->fetchAll();
+    $value = array();
+    foreach ($data as $val){
+      $value[] = $val->id;
+    }
+
+    $entities = \Drupal::entityTypeManager()
+      ->getStorage('annotation_store_entity')
+      ->loadMultiple($value);
+    $fields = array();
+    foreach ($entities as $entity){
+      $fields['annotation_key_txt_mv'][] = $entity->id();
+      $fields['annotation_txt_mv'][] = $entity->title->value;
+      $fields['annotation_type_txt_mv'][] = $entity->type->value;
+    }
+    return $fields;
+  }
+}
